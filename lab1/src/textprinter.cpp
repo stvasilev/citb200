582238782fa8ad2830b3f3df684ea4597ddda47c
St Vasilev <stenli.vasilev@gmail.com>
2019-04-07 12:11:24 +0300

    can print products and price

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -1,6 +1,10 @@
 #include "textprinter.h"
 #include <iostream>
 
-void TextPrinter::print(std::ostream &out, Invoice ) {
-    out << "OK" << std::endl;
+using std::endl;
+
+void TextPrinter::print(std::ostream &out, Invoice invoice) {
+    for (auto product : invoice.getProducts()) {
+        out << product.getName() << " (" << product.getPrice() << ") " << endl;
+    }
 }
