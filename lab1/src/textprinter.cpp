adacc562face3b0a881505bb80a57283a970daf8
St Vasilev <stenli.vasilev@gmail.com>
2019-04-07 13:37:16 +0300

    pretty

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -1,9 +1,13 @@
 #include "textprinter.h"
 #include <iostream>
+#include <iomanip>
 
 using std::endl;
 
 void TextPrinter::print(std::ostream &out, Invoice invoice) {
+    
+    out << std::fixed << std::setprecision(2);
+
     for (auto item : invoice.getItems()) {
         out << item.getQuantity() << " "
             << item.getProduct().getName() << " x"
